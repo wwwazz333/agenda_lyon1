84b587975b6e5f159c7906985a30963f0b616966
wwwazz <wwwazz333@gmail.com>
2022-09-29 16:41:04 +0200

    affichage des tach

--- lib/views/dialog/event_dialog.dart
+++ lib/views/dialog/event_dialog.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_picker/Picker.dart';
 
 import '../../model/event_calendrier.dart';
 
@@ -18,92 +15,97 @@ void showEventDialog(BuildContext context, EventCalendrier ev) {
               ev.summary,
               style: titleStyle,
             ),
-            content: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Déscription",
-                        style: titleStyle,
-                      )),
-                  Text(
-                    ev.description,
-                    style: infoStyle,
-                  ),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Salle",
-                        style: titleStyle,
-                      )),
-                  Text(
-                    ev.salle,
-                    style: infoStyle,
-                  ),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Horaire",
-                        style: titleStyle,
-                      )),
-                  Text(
-                    "${ev.date}",
-                    style: infoStyle,
-                  ),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Durée",
-                        style: titleStyle,
-                      )),
-                  Text("${ev.duree.inHours}:${ev.duree.inMinutes % 60}"),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Heures restantes",
-                        style: titleStyle,
-                      )),
-                  Text(
-                    "...",
-                    style: infoStyle,
-                  ),
-                  StatefulBuilder(
-                    builder: (context, setState) => Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Tâches",
-                              style: titleStyle,
-                            ),
-                            IconButton(
-                                onPressed: () async {
-                                  final txt = await showStringPicker(
-                                      context, "Nouvelle tâche");
-                                  if (txt != null) {
-                                    taches.add(txt);
-                                    setState(
-                                      () {},
-                                    );
-                                  }
-                                },
-                                icon: const Icon(Icons.add))
-                          ],
-                        ),
-                        ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: taches.length,
-                          itemBuilder: (context, index) => Text(taches[index]),
-                        ),
-                      ],
+            content: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Déscription",
+                          style: titleStyle,
+                        )),
+                    Text(
+                      ev.description,
+                      style: infoStyle,
                     ),
-                  )
-                ],
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Salle",
+                          style: titleStyle,
+                        )),
+                    Text(
+                      ev.salle,
+                      style: infoStyle,
+                    ),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Horaire",
+                          style: titleStyle,
+                        )),
+                    Text(
+                      "${ev.date}",
+                      style: infoStyle,
+                    ),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Durée",
+                          style: titleStyle,
+                        )),
+                    Text("${ev.duree.inHours}:${ev.duree.inMinutes % 60}"),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Heures restantes",
+                          style: titleStyle,
+                        )),
+                    Text(
+                      "...",
+                      style: infoStyle,
+                    ),
+                    StatefulBuilder(
+                      builder: (context, setState) => Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Tâches",
+                                style: titleStyle,
+                              ),
+                              IconButton(
+                                  onPressed: () async {
+                                    final txt = await showStringPicker(
+                                        context, "Nouvelle tâche");
+                                    if (txt != null) {
+                                      taches.add(txt);
+                                      setState(
+                                        () {},
+                                      );
+                                    }
+                                  },
+                                  icon: const Icon(Icons.add))
+                            ],
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics:
+                                const NeverScrollableScrollPhysics(), // géré par le scrollable parent
+                            itemCount: taches.length,
+                            itemBuilder: (context, index) =>
+                                Text(taches[index]),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             actions: [
@@ -123,6 +125,7 @@ Future<dynamic> showStringPicker(BuildContext context, String title) async {
       builder: ((context) => AlertDialog(
             title: Text(title),
             content: TextField(
+              autofocus: true,
               controller: controller,
             ),
             actions: [
